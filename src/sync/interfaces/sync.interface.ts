a2ca31101921c508e34ca06df283a50014ae59a2
amzotti <anthonymzotti@gmail.com>
2026-06-15 11:50:56 -0400

    (feat) Define external provider interfaces and types

---
+++ src/sync/interfaces/sync.interface.ts
@@ -0,0 +1,19 @@
+export enum BusinessDomain {
+    LISTINGS = 'LISTINGS',
+    COMMUNICATIONS = 'COMMUNICATIONS',
+}
+
+export interface SyncPayload {
+    id: string;
+    data: any;
+}
+
+export interface iSyncProvider {
+    readonly providerName: string;
+    readonly supportDomains: BusinessDomain[];
+
+    /** 
+     * Executes the syncrhronization logic for the external platform.
+     */
+    sync(domain: BusinessDomain, payload: SyncPayload): Promise<boolean>;
+}
